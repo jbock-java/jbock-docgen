e2ca639b4af6a434ae8597786eb7db36c9f415c1
h908714124 <kraftdurchblumen@gmx.de>
2019-05-26 08:48:36 +0200

    update jbock

--- src/main/java/com/example/helloworld/JbockAllTypes.java
+++ src/main/java/com/example/helloworld/JbockAllTypes.java
@@ -29,7 +29,7 @@ import net.jbock.Parameter;
 
 /**
  * This class contains all the basic parameter types
- * that can be used without a mapper in jbock 2.7.0.
+ * that can be used without a mapper in jbock 2.7.1.
  */
 @CommandLineArguments
 abstract class JbockAllTypes {
