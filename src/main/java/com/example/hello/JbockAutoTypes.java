591717ea28739427006b3975670d13efd31692a3
h908714124 <kraftdurchblumen@gmx.de>
2019-10-17 20:21:38 +0200

    update jbock

--- src/main/java/com/example/hello/JbockAutoTypes.java
+++ src/main/java/com/example/hello/JbockAutoTypes.java
@@ -4,20 +4,15 @@ import java.io.File;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.net.URI;
-import java.nio.charset.Charset;
 import java.nio.file.Path;
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZonedDateTime;
 import java.util.regex.Pattern;
 import net.jbock.CommandLineArguments;
 import net.jbock.Parameter;
 
 /**
  * This class contains all the basic parameter types
- * that can be used without custom mappers or collectors in jbock 2.8.2.
+ * that can be used without custom mappers or collectors in jbock 2.8.3.
  * Primitives and boxed primitives are omitted here.
  * All non-private enums can also be used.
  * For each such type X, Optional<X> defines an optional parameter,
@@ -43,14 +38,6 @@ abstract class JbockAutoTypes {
   abstract BigInteger bigInteger();
 
   /**
-   * Mapped by: java.nio.charset.Charset::forName
-   */
-  @Parameter(
-      longName = "Charset"
-  )
-  abstract Charset charset();
-
-  /**
    * Mapped by: java.io.File::new
    */
   @Parameter(
@@ -59,36 +46,12 @@ abstract class JbockAutoTypes {
   abstract File file();
 
   /**
-   * Mapped by: java.time.Instant::parse
-   */
-  @Parameter(
-      longName = "Instant"
-  )
-  abstract Instant instant();
-
-  /**
    * Mapped by: java.time.LocalDate::parse
    */
   @Parameter(
       longName = "LocalDate"
   )
   abstract LocalDate localDate();
-
-  /**
-   * Mapped by: java.time.LocalDateTime::parse
-   */
-  @Parameter(
-      longName = "LocalDateTime"
-  )
-  abstract LocalDateTime localDateTime();
-
-  /**
-   * Mapped by: java.time.OffsetDateTime::parse
-   */
-  @Parameter(
-      longName = "OffsetDateTime"
-  )
-  abstract OffsetDateTime offsetDateTime();
 
   /**
    * Mapped by: java.nio.file.Paths::get
@@ -121,12 +84,4 @@ abstract class JbockAutoTypes {
       longName = "URI"
   )
   abstract URI uRI();
-
-  /**
-   * Mapped by: java.time.ZonedDateTime::parse
-   */
-  @Parameter(
-      longName = "ZonedDateTime"
-  )
-  abstract ZonedDateTime zonedDateTime();
 }
