d22eaebd3ce11046b2f76306a2804d198916f8a7
h908714124 <kraftdurchblumen@gmx.de>
2019-11-12 23:18:46 +0100

    update jbock

--- src/main/java/com/example/hello/JbockAutoTypes.java
+++ src/main/java/com/example/hello/JbockAutoTypes.java
@@ -12,7 +12,7 @@ import net.jbock.Parameter;
 
 /**
  * This class contains all the basic parameter types
- * that can be used without custom mappers or collectors in jbock 2.9.0.
+ * that can be used without custom mappers or collectors in jbock 2.9.1.
  * Primitives and boxed primitives are omitted here.
  * All enums can also be used; they are mapped by their {@code valueOf} method.
  */
