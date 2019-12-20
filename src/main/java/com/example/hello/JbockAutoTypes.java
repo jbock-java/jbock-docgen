e7192ffbe023f1d04fb92e8774694e2d5c5692ee
h908714124 <kraftdurchblumen@gmx.de>
2019-12-20 19:53:24 +0100

    update jbock

--- src/main/java/com/example/hello/JbockAutoTypes.java
+++ src/main/java/com/example/hello/JbockAutoTypes.java
@@ -12,7 +12,7 @@ import net.jbock.Option;
 
 /**
  * This class contains all the basic parameter types
- * that can be used without custom mappers or collectors in jbock 3.4.
+ * that can be used without custom mappers or collectors in jbock 3.4.001.
  * Primitives and boxed primitives are omitted here.
  * All enums can also be used; they are mapped by their {@code valueOf} method.
  */
